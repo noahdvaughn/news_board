3ca176b27c91c572189e9833d78a62cbdf3cdcc5
Noah Vaughn <noahdvaughn@gmail.com>
2023-07-21 19:22:15 -0400

    added api route

--- client/app/search/page.tsx
+++ client/app/search/page.tsx
@@ -1,13 +1,34 @@
-import React from 'react'
+"use client"
+
+import React, { FormEvent } from 'react'
 import styles from './styles.module.css'
 import { useState } from 'react'
+import { NewsArticle } from '@/models/NewsArticles'
+import { Box, Button, Paper, TextField } from '@mui/material'
 
 const Search = () => {
-  const [searchResults, setSearchResults] = useState(null)
+  const [searchResults, setSearchResults] = useState<NewsArticle[] | null>(null)
+  const [loading, isLoading] = useState()
+  const [search, setSearch] = useState('')
+  console.log(search)
+
+  const handleSubmit = async(e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (search) {
+      alert(search)
+    }
+  }
+
+
   return (
-    <div className={styles.background}>
-      SearchPage
-    </div>
+    <Box>
+      <Box component='form' onSubmit={handleSubmit}>
+     <TextField variant='outlined' fullWidth label='search' onChange={(e)=> {setSearch(e.target.value)}} />
+     <Button type='submit'>
+      Go
+     </Button>
+      </Box>
+    </Box>
   )
 }
 
