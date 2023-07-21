90c8fe25a78b8c395edd42b759b645517c71f56f
Noah Vaughn <noahdvaughn@gmail.com>
2023-07-21 17:46:02 -0400

    added search state

--- client/app/search/page.tsx
+++ client/app/search/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import styles from './styles.module.css'
+import { useState } from 'react'
 
 const Search = () => {
+  const [searchResults, setSearchResults] = useState(null)
   return (
     <div className={styles.background}>
       SearchPage
